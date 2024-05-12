34cdbe0783560360a0854f6f8839601c6a4c49e6
Lorenzo Pecorari <pecorari.1885161@studenti.uniroma1.it>
2024-05-12 18:13:09 +0200

    [adc - fft] outliers algorithm

--- codes/adc/main/adc_utils.h
+++ codes/adc/main/adc_utils.h
@@ -4,6 +4,7 @@
 #include "driver/adc.h"
 #include "esp_adc_cal.h"
 
+#include <math.h>
 #include <inttypes.h>
 
 #define APP_NAME_ADC "[ADC] "
@@ -14,8 +15,15 @@
 static esp_adc_cal_characteristics_t adc1_chars;
 
 float* array;
-float duration;
+int duration;
 int n;
+
+float* outliers;
+
+float max = 0.0;
+float mean = 0.0;
+float std_dev = 0.0;
+float threshold = 3.0;
 
 // initialization of adc1 ch0
 void characterize_adc1_ch_0(){
@@ -29,6 +37,19 @@ void characterize_adc1_ch_0(){
     ESP_LOGI(APP_NAME_ADC, "Channel width and configuration done");
 }
 
+// calculates standard deviation of the sampled values
+float std_dev_calc(){
+    float std_dev = 0;
+
+    for(int i = 0; i < n; i++){
+        std_dev += pow(array[i] - mean, 2);
+    }
+
+    std_dev = sqrt(std_dev / n);
+
+    return std_dev;
+}
+
 // gets values from the adc and puts
 // them into a buffer passed as argument of the function
 void get_values_from_adc(){
@@ -37,15 +58,66 @@ void get_values_from_adc(){
     else if(!n)
         ESP_LOGE(APP_NAME_ADC, "No samples!");
     else{
+        int period = pdMS_TO_TICKS(duration);
+
         for(int i = 0; i < n; i++){
             array[i] = esp_adc_cal_raw_to_voltage(adc1_get_raw(ADC1_CHANNEL_0), &adc1_chars);
             //ESP_LOGI(APP_NAME_ADC, "Sampled: %f mV at index %d", array[i], i);
          
-            vTaskDelay(duration);
+            mean += array[i];
+
+            if(array[i] > max)
+                max = array[i];
+            
+            vTaskDelay(period);
             // delay to define in order to achieve a different sampling frequency    
         }
 
+        mean = (float) (mean / n);
+        std_dev = std_dev_calc();
+        printf("Max: %f - Average : %f - Stadnard dev: %f - threshold; %f\n", max, mean, std_dev, threshold);
+
         ESP_LOGI(APP_NAME_ADC, "Sampling completed");
     }
-
 }
+
+void outliers_calc(){
+    outliers = (float*) malloc(sizeof(float) * (n / 2));
+
+    if(!outliers)
+        ESP_LOGE(APP_NAME_ADC, "Outliers memory allocation error");
+    
+    int idx = 0;
+    float z = 0.0;
+
+    for(int i = 0; i < n && idx < (n / 2); i++){
+        z = (array[i] - mean) / std_dev;
+
+        if((z > threshold) || (z < (-threshold))){
+            outliers[idx] = array[i];
+            ESP_LOGI(APP_NAME_ADC, "Outlier %d : %f", idx, outliers[idx]);
+            idx++;
+        }
+    }
+
+    free(outliers);
+}
+
+
+/*
+    === NOTES FOR FFT ===
+
+# New duration of delay:
+    Since duration gives the information on how many ticks are needed to wait for sampling with a certain frequency,
+    the sampling frequency to use for a more accurate approximation of the signal have to be 2 * f_max.
+    The new duration to pass to the adc function for sampling the signal have to be duration = 1 / f_sampling =
+    = 1 / (2 * f_max) = 1 / (2 * idx * n / (n * duration)) = 1 / (2 * idx / duration) = duration / (2 * duration)
+
+# Trashold value:
+    Obtaining a relevant value for the calculation of the max frequency to use for finding the new sampling frequqncy
+    can be achieved through the z-score; calculating the z-score of a value tells if it is an outlier or not.
+
+    z = (value - mean) / std_dev -> if z > threshold so value is an outlier
+
+    Theshold value is defined as the mean value plus/minus 3 scale order of standard deviation as base point (statistical approach).
+*/
--- codes/adc/main/main.c
+++ codes/adc/main/main.c
@@ -8,12 +8,19 @@ void app_main(void){
     if(!array)
         ESP_LOGE(APP_NAME_ADC, "Error during malloc!");
     else{
-        duration = (float) (0.01 / portTICK_PERIOD_MS);
+        // duration = 1000 / (float) portTICK_PERIOD_MS;
+        // printf("Duration: %f - %fs\n", (duration / 100), (float) portTICK_PERIOD_MS);
+        // printf("Sampling frequency : %f Hz\n", (float) (N * 10 / duration));
+
+        duration = 1;
+        printf("Duration: %d (ticks) - Sampling frequency : idk (1 / ticks)\n", duration);
+        
         n = N;
-        printf("Sampling frequency : %f\n", (float) (1 / duration));
 
         characterize_adc1_ch_0();
         get_values_from_adc();
+
+        outliers_calc();
 
         free(array);
     }
